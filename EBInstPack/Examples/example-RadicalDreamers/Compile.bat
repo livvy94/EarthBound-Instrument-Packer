b11713020b628c80c4da1b56d44a09b8141da23e
vince94 <12562322+vince94@users.noreply.github.com>
2021-09-05 17:26:20 -0400

    Make some tweaks to the examples

---
+++ EBInstPack/Examples/example-RadicalDreamers/Compile.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Users\vince\Dropbox\Programming scratchpad\EarthBound-Instrument-Packer\Release Dump\EBInstPack.exe" "C:\Users\vince\Dropbox\Programming scratchpad\EarthBound-Instrument-Packer\EBInstPack\Examples\example-RadicalDreamers"
