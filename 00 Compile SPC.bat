60b4f30388b5b5d2ce577455c11344ab4c7f1f9a
livvy94 <haxxor199x@gmail.com>
2025-03-17 07:50:28 -0400

    Add batch file

---
+++ 00 Compile SPC.bat
@@ -0,0 +1,6 @@
+@echo off
+"C:\Users\vince\Dropbox\Programming scratchpad\EarthBound-Instrument-Packer\Release Dump\EBInstPack.exe" "%CD%"
+
+REM Put the path to EBInstPack.exe here, and put this batch file into the same
+REM directory as the BRRs and config of a song you're working on.
+REM Double-clicking this batch file will compile any .ebm files into an SPC!
